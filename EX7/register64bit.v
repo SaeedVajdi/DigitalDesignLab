b0a10ee1879ec648a43cef5b45f62fe0e4ec7d58
SaeedVajdi <80834424+SaeedVajdi@users.noreply.github.com>
2021-11-30 23:59:38 +0330

    Add files via upload

--- EX7/register64bit.v
+++ EX7/register64bit.v
@@ -1,12 +1,12 @@
-module Register64Bit(input [63:0] data, input reset, load, clock, output wire [63:0] out);
+module Register64Bit(input [63:0] data, input reset, load, clock, output reg [63:0] out);
 
     always @(posedge clock or posedge reset) begin
         if (reset) begin
-            out <= 0;
+            out = 0;
         end else if (load) begin
-            out <= data;
+            out = data;
         end else begin
-            out <= out;
+            out = out;
         end
     end
 
@@ -15,27 +15,27 @@ module testbench;
 
     reg [63:0] data;
     reg reset, load, clock;
-    wire [63:0] out;
+    reg [63:0] out;
 
     Register64Bit r(.data(data), .reset(reset), .load(load), .clock(clock), .out(out));
 
     always #20 clock = ~clock;
     initial begin
-        data <= 'h1;
-        reset <= 0;
-        load <= 0;
-        clock <= 0;
-        out <= 0;
+        data = 'h1;
+        reset = 0;
+        load = 0;
+        clock = 0;
+        out = 0;
     end
     initial begin
 
-        reset <= 0;
-        #10 reset <= 1;
+        reset = 0;
+        #10 reset = 1;
         repeat (7) @(posedge clock)
-            data <= ~data;
-        #10 load <= 1;
+            data = ~data;
+        #10 load = 1;
         repeat (7) @(posedge clock)
-            data <= ~data;
+            data = ~data;
 
         $finish;
     end
