b0c434b5a2a0fbcf1bfcb9e2e32615593fedb22b
SaeedVajdi <80834424+SaeedVajdi@users.noreply.github.com>
2021-12-11 14:28:51 +0330

    Add files via upload

---
+++ EX8/Test Bench.v
@@ -0,0 +1,22 @@
+module test_Risc_16_bit;
+
+    // Inputs
+    reg clk;
+
+    Risc_16_bit uut(
+        .clk(clk)
+    );
+
+    initial
+        begin
+            clk <= 0;
+            #160
+            $finish;
+        end
+
+    always
+        begin
+            #5 clk = ~clk;
+        end
+
+endmodule
